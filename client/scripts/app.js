7813bb789580d84df3396248e38f92edd0632a2a
George gd Fekaris <gdfekaris@gmail.com>
2018-09-15 12:29:17 -0700

    made progress on room functionality: re-designed and appended exisitng rooms to select button

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,38 +1,49 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $.ajax({
     url: `http://parse.rpt.hackreactor.com/chatterbox/classes/messages`,
     type: `GET`,
-    error: function() {
+    error: function () {
       $('#chats').html('<p>An error has occurred</p>');
     },
     dataType: 'json',
-    success: function(data) {
+    success: function (data) {
       console.log(data);
-      //let rooms = {};
+
+      let rooms = {};
 
       for (var i = data.results.length - 1; i >= 0; i--) {
         var $message = $(`<div class="chat ${data.results[i].roomname}">`).html(
           `<span class="username">${data.results[i].username}:</span>
             <span class="messageText">${data.results[i].text}</span>`
         );
-        // var $description = $(`<p class=${data.results[i].roomname}>`).text(
-        //   data.results[i].text
-        // );
 
-        // if (!rooms[data.results[i].roomname]) {
-        //   rooms[data.results[i].roomname] = data.results[i].roomname)
-        // }s
-        console.log('CLASS =', $message.className);
+        if (!rooms[data.results[i].roomname]) {
+          rooms[data.results[i].roomname] = data.results[i].roomname;
+        }
+
         $('#chats').append($message);
-        //.append($description);
       }
 
-      $('.username').on('click', function() {
+
+      for (var key in rooms) {
+        var $option = $(`<option>${rooms[key]}</option>`)
+        $('#dropDown').append($option);
+      }
+
+
+      $('.username').on('click', function () {
         console.log(`added friend!`);
       });
     }
   });
 });
+
+$(document).on('change', '#dropDown', function() {
+  var $selected = $(`#dropDown option:selected`);
+  var $selectedText= $selected.text()
+  console.log($selectedText);
+});
+
 
 // $.ajax({
 //   // This is the url you should use to communicate with the parse API server.
